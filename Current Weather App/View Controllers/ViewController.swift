34707482d9d4f8af5e6ce52470f8367285812ac9
Rusi0991 <ruslanismayilov484@gmail.com>
2022-02-24 17:25:46 -0800

    Current Weather App UI created

---
+++ Current Weather App/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Current Weather App
+//
+//  Created by Ruslan Ismayilov on 2/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
