05bdb6c068b059001ea9b6107c994723d9c50756
Rusi0991 <ruslanismayilov484@gmail.com>
2022-02-27 19:45:52 -0800

    Menu button implemented

--- Current Weather App/View Controllers/ViewController.swift
+++ Current Weather App/View Controllers/ViewController.swift
@@ -11,6 +11,9 @@ class ViewController: UIViewController {
     @IBOutlet var mainView: UIView!
     
     @IBOutlet weak var hamburgerView: UIView!
+    
+    @IBOutlet weak var hamburgerViewLeadingC: NSLayoutConstraint!
+    @IBOutlet weak var hamburgerViewTrailingC: NSLayoutConstraint!
     @IBOutlet weak var weatherIconImageView: UIImageView!
     
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -18,16 +21,37 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cityLabel: UILabel!
     
+    var menuOut = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         hamburgerView.isHidden = true
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        hamburgerView.isHidden = true
+    }
 
+    @IBAction func menuButtonClicked(_ sender: Any) {
+        hamburgerView.isHidden = false
+        if menuOut == false{
+//            hamburgerViewLeadingC.constant = 0
+//            hamburgerViewTrailingC.constant = 0
+            menuOut = true
+        } else {
+//            hamburgerViewLeadingC.constant = 0
+//            hamburgerViewTrailingC.constant = 0
+            menuOut = false
+            hamburgerView.isHidden = true
+        }
+        
+        
+    }
     @IBAction func seacrhButtonTapped(_ sender: Any) {
         self.presentSearchAlertController(title: "Enter city name", message: nil, style: .alert)
     }
     
-    @IBAction func menuTapped(_ sender: Any) {
-    }
+   
+    
 }
-
