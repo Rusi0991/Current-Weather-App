e178ff9bf219e9c8a828fed6c01f8ce62681c905
Rusi0991 <ruslanismayilov484@gmail.com>
2022-03-09 15:13:52 -0800

    Added and wired objects to ConverterVC

--- Current Weather App/View Controllers/ConverterViewController.swift
+++ Current Weather App/View Controllers/ConverterViewController.swift
@@ -8,22 +8,19 @@
 import UIKit
 
 class ConverterViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var celsiusLabel: UILabel!
+    
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    
+    @IBOutlet weak var slider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
     }
-    */
-
 }
